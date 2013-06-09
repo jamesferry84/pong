8547d6c3e2448d3e0cf847e91720720f65d3a47d
James Ferry <jrferry@gmail.com>
2013-06-09 13:25:35 +0100

    First Commit

---
+++ Repos/.svn/pristine/60/608b17ab64cc564e560da3486cd8ffc3c9c51316.svn-base
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+namespace TutorialGame1
+{
+    class Ball
+    {
+        Texture2D texture;
+        Vector2 position;
+        float speed;
+        Vector2 direction;
+        Rectangle rect;
+
+        public Rectangle Rect
+        {
+            get { return this.rect; }
+            set { rect = value; }
+        }
+
+        public Vector2 Direction
+        {
+            get { return this.direction; }
+            set { direction = value; }
+        }
+
+        public Vector2 Position
+        {
+            get { return this.position; }
+            set { position = value; }
+        }
+
+        public Ball(ContentManager Content, string asset, Vector2 position)
+        {
+            texture = Content.Load<Texture2D>(asset);
+            this.position = position;
+            speed = 3f;
+            direction = new Vector2(-1, -1);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            if (position.Y <= 0)
+            {
+                direction.Y *= -1;
+            }
+            if (position.Y + texture.Height >= 600)
+            {
+                direction.Y *= -1;
+            }
+
+                position += speed * direction;
+
+            rect = new Rectangle((int)position.X, (int)position.Y, texture.Width, texture.Height);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, rect, Color.White);
+        }
+    }
+}
---
+++ Repos/TutorialGame1/TutorialGame1/Paddle.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace TutorialGame1
+{
+    class Paddle
+    {
+        Texture2D texture;
+        Vector2 position;
+        float speed;
+        Rectangle rect;
+        bool isPlayer;
+
+        KeyboardState keyState;
+
+        public Rectangle Rect
+        {
+            get { return this.rect; }
+            set { rect = value; }
+        }
+
+        public Paddle(ContentManager Content, string asset, Vector2 position, bool isPlayer)
+        {
+            texture = Content.Load<Texture2D>(asset);
+            this.position = position;
+            speed = 10f;
+            this.isPlayer = isPlayer;
+        }
+
+
+        public void Update(GameTime gametime, Ball ball)
+        {
+            keyState = Keyboard.GetState();
+
+            if (isPlayer)
+            {
+                if (keyState.IsKeyDown(Keys.Down) && (position.Y <= 600 - texture.Height))
+                {
+                    position.Y += speed;
+                }
+                if (keyState.IsKeyDown(Keys.Up) && position.Y > 0)
+                {
+                    position.Y -= speed;
+                }
+            }
+            else
+            {
+                if (ball.Position.Y >= position.Y)
+                {
+                    position.Y += speed;
+                }
+                if (ball.Position.Y <= position.Y)
+                {
+                    position.Y -= speed;
+                }
+            }
+
+            rect = new Rectangle((int)position.X, (int)position.Y, texture.Width, texture.Height);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, rect, Color.White);
+        }
+    }
+}
